5a80f29615c32678950ee29d869e55e3bbc7c500
Adam Kurowski <adam@kurowski.co.uk>
2018-01-27 23:06:56 +0000

    Fixed error.

--- area of a circle.py
+++ area of a circle.py
@@ -2,22 +2,22 @@ from math import pi
 from sys import exit
 # this imports the math library, which contains various mathematical functions
 
-def intCheck (input):
-    if str.isdigit(input): # The str.isdigit() method is detailed here: https://www.tutorialspoint.com/python/string_isdigit.htm
-      radius = int(radius)
-    else:
-      exit("Invalid entry")
-
-# Defines a function that takes an input ('input') and runs an 'if' statement.
-# If the input is a 'digit', the function converts the variable 'radius' into
-# an integer. If the input is not a 'digit' then the program is terminated.
-
 radius = input(">Enter circle radius in meters: ")
 # what follows the '=' sign assigns that value to the variable 'radius'.
 # The 'input' command lets the program know to wait for the user to input 
 # some value.
 
-intCheck(radius)
+def intCheck (input):
+    if str.isdigit(input): # The str.isdigit() method is detailed here: https://www.tutorialspoint.com/python/string_isdigit.htm
+      return int(input)
+    else:
+      exit("Invalid entry")
+
+# Defines a function that takes an input ('input') and runs an 'if' statement.
+# If the input is a 'digit', the function returns the input converted to
+# an integer. If the input is not a 'digit' then the program is terminated.
+
+radius = intCheck(radius)
 # This is running the variable 'radius' through the intCheck function we defined earlier.
 
 area = pi*radius**2
@@ -25,5 +25,5 @@ area = pi*radius**2
 # library and it is multiplied by the square of radius. Note the use of
 # the ** to raise radius to an index of 2
 
-print ("The area of a circle with radius", radius, "is", area, "square meters")
+print ("The area of a circle with a radius of", radius, "meters is", area, "square meters")
 # Prints the result
