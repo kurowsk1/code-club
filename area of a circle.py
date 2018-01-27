e1bdb038ac838d5f85e7e665f3575d324779c5b2
Adam Kurowski <adam@kurowski.co.uk>
2018-01-27 19:25:39 +0000

    Check function

--- area of a circle.py
+++ area of a circle.py
@@ -1,10 +1,19 @@
-import math
+from math import pi
+from sys import exit
 # this imports the math library, which contains various mathematical functions
 
 radius = input(">Enter circle radius in meters: ")
 # what follows the '=' sign assigns that value to the variable 'radius'.
 # The 'input' command lets the program know to wait for the user to input 
 # some value.
+
+def intCheck (input):
+    if str.isdigit(input):
+      pass
+    else:
+      exit("Invalid entry")
+
+intCheck(radius)
 
 radius = int(radius)
 # Converts the variable 'radius' from a string to an integer.
@@ -15,7 +24,7 @@ radius = int(radius)
 # without '' marks, instructing the program to to print the variable's
 # value; not the variable name.
 
-area = math.pi*radius**2
+area = pi*radius**2
 # This defines a new variable; area. The pi function is used from the math
 # library and it is multiplied by the square of radius. Note the use of
 # the ** to raise radius to an index of 2
